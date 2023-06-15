03daad5c910183f3fab2a9c10fec2060673917fe
Mohamed Nabil <mamadonabil919@gmail.com>
2023-06-15 04:55:29 +0200

    add api. + lambda + auth + graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "inspirationalquotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "inspirationalquotes71ef8e71": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inspoquoteLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements';
+//components
+import { GradientBackgroundCon , BackgroundImage1 ,BackgroundImage2,FooterCon,RedSpan,FooterLink ,QuoteGeneratorCon,QuoteGeneratorInnerCon,QuoteGeneratorTitle,QuoteGeneratorSubTitle,GenerateQuoteButton,GenerateQuoteButtonText} from '@/components/QuoteGenerator/QuoteGeneratorElements';
 
-
+//Assets
+import Clouds1 from '../assets/cloud-and-thunder.png'
+import Clouds2 from '../assets/cloudy-weather.png'
 
 export default function Home() {
+    const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
     return(
         <>
         <Head>
@@ -16,7 +21,52 @@ export default function Home() {
       </Head>
          {/* Background */}
          <GradientBackgroundCon>
-            
+
+          {/* Quote Generator Modal Pop-up */ }
+          {/* <QuoteGeneratorModal
+          /> */}
+
+            {/* Quote Generator */ }
+           <QuoteGeneratorCon>
+               <QuoteGeneratorInnerCon>
+                 <QuoteGeneratorTitle>
+                    Daily Inspiration Generator
+                 </QuoteGeneratorTitle>
+                 <QuoteGeneratorSubTitle>
+                 Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+                 </QuoteGeneratorSubTitle>
+                 <GenerateQuoteButton >
+              <GenerateQuoteButtonText>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+               </QuoteGeneratorInnerCon>
+           </QuoteGeneratorCon>
+
+
+
+
+         {/* Background images */ }
+         <BackgroundImage1 
+          src={Clouds1}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <BackgroundImage2 
+          src={Clouds2}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        {/* Footer */ }
+        <FooterCon>
+            <>
+            Quotes Generated: {numberOfQuotes}
+            <br/>
+            Developed with <RedSpan>♥</RedSpan> by <FooterLink href="https://www.linkedin.com/in/mohamed-nabiil/" target="_blank" rel="noopener noreferrer"> @MohamedNabil </FooterLink>
+            </>
+        </FooterCon>
          </GradientBackgroundCon>
       
         </>
